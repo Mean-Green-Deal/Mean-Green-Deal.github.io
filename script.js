b6c6f7a12f4e9d30c2a7465e2ab24e0f862f5b83
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-01-31 16:36:43 -0600

    disable google controls

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: 33.20750461273979, lng: -97.15295817275108 },
       zoom: 14.7,
-      mapid: '576037077fe48406'
+      mapid: '576037077fe48406',
+      mapTypeControl: false,
+      fullscreenControl: false,
+      streetViewControl: false
     });
     new google.maps.Marker({
         position: { lat: 33.20750461273979, lng: -97.15295817275108 },
