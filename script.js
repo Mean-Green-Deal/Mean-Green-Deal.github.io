34d5e7cda126ddba084856a82ebe35fcf1be8bc9
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-01-31 16:14:23 -0600

    map marker

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ function initMap() {
       zoom: 14.7,
       mapid: '576037077fe48406'
     });
+    new google.maps.Marker({
+        position: { lat: 33.20750461273979, lng: -97.15295817275108 },
+        map,
+        title: "Recycling Bin 1",
+    });
   }
 
   //33.20750461273979, -97.15295817275108
