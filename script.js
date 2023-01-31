0bab0f3db346ec9e066fbb16ad27565798b25899
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-01-31 16:48:35 -0600

    tryin to change size

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function initMap() {
         position: { lat: 33.20750461273979, lng: -97.15295817275108 },
         map,
         title: "Recycling Bin 1",
-        scale: 2,
+        size: new google.maps.Size(5, 4),
         icon: {
             url: "bin.svg",
             //scaleSize: new google.maps.Size(38, 31)
