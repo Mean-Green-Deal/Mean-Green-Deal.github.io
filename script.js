18d5c20a94d8d6c3d82b11b8faf5619ce323dddb
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-01-31 16:42:38 -0600

    changed scale

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ function initMap() {
         position: { lat: 33.20750461273979, lng: -97.15295817275108 },
         map,
         title: "Recycling Bin 1",
+        scale: 2,
         icon: {
             url: "bin.svg",
-            scaleSize: new google.maps.Size(38, 31)
+            //scaleSize: new google.maps.Size(38, 31)
+            
         }
     });
   }
