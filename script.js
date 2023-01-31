6e83415601f083c4e259e8eb852abfb51100e6bf
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-01-31 17:23:40 -0600

    sizeeeeee

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ function initMap() {
         position: { lat: 33.20750461273979, lng: -97.15295817275108 },
         map,
         title: "Recycling Bin 1",
-        size: new google.maps.Size(5, 4),
         icon: {
             url: "bin.svg",
             //scaleSize: new google.maps.Size(38, 31)
+            size: new google.maps.Size(5, 4)
             
         }
     });
