b1c6587e30d36b2404ad8efed2c8ae0464494447
Reagan S <86692575+ReaganStephens@users.noreply.github.com>
2023-02-17 15:16:42 -0600

    fixed logins again

--- src/main.js
+++ src/main.js
@@ -39,8 +39,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelectorAll(".form__input").forEach(inputElement => {
         inputElement.addEventListener("blur", e => {
-            if (e.target.id == "signupUsername" && e.target.value.link > 0 && e.target.value.length < 4) {
-                setInputError(inputElement, "Username must be 4 characters in length");
+            if (e.target.id == "signupUsername" && e.target.value.length > 0 && e.target.value.length < 4) {
+                setInputError(inputElement, "Username must be at least 4 characters in length");
             }
         });
     });
