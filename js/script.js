23de5ee7c68bcca1741b7d839f0a2d41dbb5bdfb
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-02-22 10:28:42 -0600

    stry react to delete

--- js/script.js
+++ js/script.js
@@ -59,9 +59,6 @@ new google.maps.Marker({
   }
 });
 }
-const domContainer = document.querySelector('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
   //svg image url: https://www.recycling.com/wp-content/uploads/2016/06/recycling-symbol-icon-solid-dark-blue.png
   //33.20750461273979, -97.15295817275108
 
