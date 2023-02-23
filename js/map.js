11f91c96f1eda5b86b538b39b4b20a0081fcd0cc
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-02-23 12:07:39 -0600

    cleaned map markers

--- js/map.js
+++ js/map.js
@@ -15,6 +15,33 @@ function initMap() {
         }
       ]
     });
+    const bins = [
+      ["Recycling Bin DP Out 1", 33.2555149, -97.1529043],
+      ["Recycling Bin DP OUT 2", 33.2548833, -97.1533700],
+      ["Recycling Bin DP IN 3", 33.2543800, -97.1536788],
+      ["Recycling Bin DP IN 4", 33.2546405, -97.1535286],
+      ["Recycling Bin DP IN 5", 33.2541964, -97.1537884],
+      ["Recycling Bin DP OUT 6", 33.2540419, -97.1528614],
+      ["Recycling Bin DP IN 7", 33.2530084, -97.1520399],
+    ];
+    for (let i = 0; i < bins.length; i++) {
+      const bins = bins[i];
+  
+      new google.maps.Marker({
+        position: { lat: bins[1], lng: beach[2] },
+        map,
+        icon: {
+          url: "pictures/bin.svg",
+          scaledSize: new google.maps.Size(28.5, 23.25) 
+        },
+        shape: shape,
+        title: bins[0],
+      });
+    }
+  }
+
+
+    /*
     new google.maps.Marker({
         position: { lat: 33.2555149, lng: -97.1529043 },
         map,
@@ -65,7 +92,35 @@ new google.maps.Marker({
       
   }
 });
-}
+new google.maps.Marker({
+  position: { lat: 33.2540419, lng: -97.1528614 },
+  map,
+  title: "Recycling Bin DP OUT 6",
+  icon: {
+      url: "pictures/bin.svg",
+      scaledSize: new google.maps.Size(28.5, 23.25)
+  }
+});
+new google.maps.Marker({
+  position: { lat: 33.2530084, lng: -97.1520399 },
+  map,
+  title: "Recycling Bin DP IN 7",
+  icon: {
+      url: "pictures/bin.svg",
+      scaledSize: new google.maps.Size(28.5, 23.25)
+  }
+});
+new google.maps.Marker({
+  position: { lat: 33.2530084, lng: -97.1520399 },
+  map,
+  title: "Recycling Bin DP IN 7",
+  icon: {
+      url: "pictures/bin.svg",
+      scaledSize: new google.maps.Size(28.5, 23.25)
+  }
+});
+*/
+
   //svg image url: https://www.recycling.com/wp-content/uploads/2016/06/recycling-symbol-icon-solid-dark-blue.png
   //33.20750461273979, -97.15295817275108
 
