211fe1036a3d4621c57def24b05717bdf54eeb9f
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-03-21 16:18:15 -0500

    FB

--- js/login.js
+++ js/login.js
@@ -1,178 +1,3 @@
-const  { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } = require("firebase/auth");
-const  { initializeApp } = require("firebase/app");
-const  { getAnalytics } = require("firebase/analytics");
-
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
-
-    messageElement.textContext = message;
-    messageElement.classList.remove("form__message--success", "form_message-error");
-    messageElement.classList.add('form__message--${type}');
-}
-
-//setFormMessage(loginFoirm, "success", "You're logged in!");
-
-function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-}
-
-function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input-error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
-
-}
-
-var LoginInfo = [
-    {
-        email: "robertrstephens7@gmail.com",
-        username: "reagan",
-        password: "test"
-    }
-]
-
-//regex to check if email contains @
-function emailRegex(input) {
-    let regex = /@/i;
-    return regex.test(input);
-}
-
-
-//retrieves user info for login
-function login() {
-    var username = document.getElementById("username").value
-    var password = document.getElementById("password").value
-    
-    for (i = 0; i <LoginInfo.length; i++) {
-        if (username == LoginInfo[i].username && password == LoginInfo[i].password) {
-            alert(username + " is logged in")
-            //FB
-            signInWithEmailAndPassword(registerPassword, registerEmail)
-            //FB
-            return
-        }
-    }
-    console.log("incorrect username or password")
-}
-
-function registerUser() {
-    var registerUser = document.getElementById("newUser").value
-    var registerEmail = document.getElementById("newEmail").value
-    var registerPassword = document.getElementById("newPassword").value
-    var registerConfirmPassword = document.getElementById("confirmPassword").value
-
-
-///////////////////////////////Valid Credentials ///////////////////////////////////////////
-
-if (registerUser.length < 3) {
-    alert("That username is too short.")
-    return
-}
-
-else if (registerEmail.length < 3) {
-    alert("That email is too short.")
-    return
-}
-
-else if(emailRegex(registerEmail) == false) {
-    alert("Invalid email.")
-    return
-}
-
-else if (registerPassword.length < 3) {
-    alert("That password is too short.")
-    return
-}
-
-for (i = 0; i <LoginInfo.length; i++){
-    if(registerUser==LoginInfo[i].username){
-        alert("That username is already in use, choose another")
-        return
-    }
-}
-
-for (i = 0; i <LoginInfo.length; i++){
-    if(registerEmail==LoginInfo[i].email){
-        alert("That email is already in use, choose another")
-        return
-    }
-}
-
-if (registerPassword == registerConfirmPassword)
-{
-    //FB
-    createUserWithEmailAndPassword(registerPassword, registerEmail)
-    //FB
-    var newUser = {
-        username: registerUser,
-        password: registerPassword,
-        email: registerEmail
-    
-    }
-    LoginInfo.push(newUser)
-    alert("Your Account has been created!")
-    return
-}
-else{
-    alert("Passwords do not match. Try again")
-    return
-}
-
-
-}
-///////////////////////////////Valid Credentials///////////////////////////////////////////
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-    
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-    });
-
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
-    });
-
-    //Tutorial's login method.
-/*
-    loginForm.addEventListener("submit", e=> {
-        e.preventDefault();
-
-        // Perform your AJAX/Fetch login
-
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
-    });
-
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id == "newUser" && e.target.value.length > 0 && e.target.value.length < 4) {
-                setInputError(inputElement, "Username must be at least 4 characters in length");
-            }
-        });
-
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        })
-    });
-    */
-   
-});
-/*
-//FIREBASE DATABASE AUTHENTICATION
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAFvVTARYzQrWvE9OXCTY3JV3o9SxHbJ7U",
   authDomain: "mean-green-deal-726f9.firebaseapp.com",
@@ -182,65 +7,126 @@ const firebaseConfig = {
   appId: "1:747867835951:web:084db4a1feb703eafe00da",
   measurementId: "G-2QKNB5QXF4"
 };
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+firebase.initializeApp(firebaseConfig);
+// Initialize variables
+const auth = firebase.auth()
+const database = firebase.database()
 
-onAuthStateChanged(auth, user => {
-    if(user != null){
-        console.log('logged in!');
-    }else{
-        console.log('No user');
+// Set up our register function
+function register () {
+  // Get all our input fields
+  email = document.getElementById('email').value
+  password = document.getElementById('password').value
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Email or Password is Outta Line!!')
+    return
+    // Don't continue running the code
+  }
+  // Move on with Auth
+  auth.createUserWithEmailAndPassword(email, password)
+  .then(function() {
+    // Declare user variable
+    var user = auth.currentUser
+
+    // Add this user to Firebase Database
+    var database_ref = database.ref()
+
+    // Create User data
+    var user_data = {
+      email : email,
+      last_login : Date.now()
     }
-});
-*/
-//FB
-//import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-//import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
+
+    // Push to Firebase Database
+    database_ref.child('users/' + user.uid).set(user_data)
+
+    // DOne
+    alert('User Created!!')
   })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
-  
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
+  .catch(function(error) {
+    // Firebase will use this to alert of its errors
+    var error_code = error.code
+    var error_message = error.message
+
+    alert(error_message)
   })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+}
 
-// Import the functions you need from the SDKs you need
-//import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
-//import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Set up our login function
+function login () {
+  // Get all our input fields
+  email = document.getElementById('email').value
+  password = document.getElementById('password').value
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAFvVTARYzQrWvE9OXCTY3JV3o9SxHbJ7U",
-  authDomain: "mean-green-deal-726f9.firebaseapp.com",
-  projectId: "mean-green-deal-726f9",
-  storageBucket: "mean-green-deal-726f9.appspot.com",
-  messagingSenderId: "747867835951",
-  appId: "1:747867835951:web:084db4a1feb703eafe00da",
-  measurementId: "G-2QKNB5QXF4"
-};
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Email or Password is Outta Line!!')
+    return
+    // Don't continue running the code
+  }
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
+  auth.signInWithEmailAndPassword(email, password)
+  .then(function() {
+    // Declare user variable
+    var user = auth.currentUser
+
+    // Add this user to Firebase Database
+    var database_ref = database.ref()
+
+    // Create User data
+    var user_data = {
+      last_login : Date.now()
+    }
+
+    // Push to Firebase Database
+    database_ref.child('users/' + user.uid).update(user_data)
+
+    // DOne
+    alert('User Logged In!!')
+
+  })
+  .catch(function(error) {
+    // Firebase will use this to alert of its errors
+    var error_code = error.code
+    var error_message = error.message
+
+    alert(error_message)
+  })
+}
+
+// Validate Functions
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/
+  if (expression.test(email) == true) {
+    // Email is good
+    return true
+  } else {
+    // Email is not good
+    return false
+  }
+}
+
+function validate_password(password) {
+  // Firebase only accepts lengths greater than 6
+  if (password < 6) {
+    return false
+  } else {
+    return true
+  }
+}
+
+function validate_field(field) {
+  if (field == null) {
+    return false
+  }
+
+  if (field.length <= 0) {
+    return false
+  } else {
+    return true
+  }
+}
+
