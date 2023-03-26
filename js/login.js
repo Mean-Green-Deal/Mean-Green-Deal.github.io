c41c468ca3d31516253264c8205fe3d80e74b394
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-03-26 13:12:50 -0500

    test

--- js/login.js
+++ js/login.js
@@ -41,7 +41,7 @@ const firebaseConfig = {
       
           // DOne
           alert('User Logged In!!')
-          window.location.href = "https://mean-green-deal.github.io/content/userloggedin.html";
+          //window.location.href = "https://mean-green-deal.github.io/content/userloggedin.html";
       })
           .catch(function(error) {
               // Firebase will use this to alert of its errors
