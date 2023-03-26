058ef1a2b6171df73ae88341e333cfa004534ebc
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-03-25 19:38:02 -0500

    refactor (moved field)

--- js/login.js
+++ js/login.js
@@ -13,7 +13,210 @@ const firebaseConfig = {
      const auth = firebase.auth()
      const database = firebase.database()
 
-
+     function login() {
+      var email = document.getElementById("email").value
+      var password = document.getElementById("password").value
+  
+      if (validate_email(email) == false || validate_password(password) == false) {
+          alert('Email or Password is Outta Line!!')
+          return
+          // Don't continue running the code
+        }
+      
+        auth.signInWithEmailAndPassword(email, password)
+        .then(function() {
+          // Declare user variable
+          var user = auth.currentUser
+      
+          // Add this user to Firebase Database
+          var database_ref = database.ref()
+      
+          // Create User data
+          var user_data = {
+            last_login : Date.now()
+          }
+      
+          // Push to Firebase Database
+          database_ref.child('users/' + user.uid).update(user_data)
+      
+          // DOne
+          alert('User Logged In!!')
+          window.location.href = "https://mean-green-deal.github.io/content/userloggedin.html";
+      })
+          .catch(function(error) {
+              // Firebase will use this to alert of its errors
+              var error_code = error.code
+              var error_message = error.message
+          
+              alert(error_message)
+              window.location.href = "https://mean-green-deal.github.io";
+            })
+      /*
+      for (i = 0; i <LoginInfo.length; i++) {
+          if (username == LoginInfo[i].username && password == LoginInfo[i].password) {
+              alert(username + " is logged in")
+            //FB
+            auth.signInWithEmailAndPassword(username, password)
+            .then(function() {
+              // Declare user variable
+              var user = auth.currentUser
+  
+              // Add this user to Firebase Database
+              var database_ref = database.ref()
+  
+              // Create User data
+              var user_data = {
+                last_login : Date.now()
+              }
+              // Push to Firebase Database
+              database_ref.child('users/' + user.uid).update(user_data)
+            })
+            .catch(function(error) {
+              // Firebase will use this to alert of its errors
+              var error_code = error.code
+              var error_message = error.message
+  
+              alert(error_message)
+            })
+             window.location.href = "https://mean-green-deal.github.io/";
+              return
+          }
+      }
+      */
+      //console.log("incorrect username or password")
+  }
+  
+  function registerUser() {
+    var registerUser = document.getElementById("newUser").value
+    var registerEmail = document.getElementById("newEmail").value
+    var registerPassword = document.getElementById("newPassword").value
+    var registerConfirmPassword = document.getElementById("confirmPassword").value
+    auth.createUserWithEmailAndPassword(registerEmail, registerPassword)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        registerEmail : registerEmail,
+        registerUser : registerUser,
+        last_login : Date.now()
+      }
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).set(user_data)
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+      alert(error_message)
+    })
+  
+  
+  
+  
+      //FB
+      /*
+      auth.createUserWithEmailAndPassword(registerEmail, registerPassword)
+      .then(function(userCredential) {
+          const user = userCredential.user;
+          const usersRef = db.collection('users');
+          var user_data = {
+              registerEmail : registerEmail,
+              registerUser : registerUser,
+              last_login : Date.now()
+            };
+            usersRef.add(user_data)
+          
+        })
+          /*
+        // Declare user variable
+        
+        var user = auth.currentUser
+    
+        // Add this user to Firebase Database
+        var database_ref = database.ref()
+    
+        // Create User data
+        var user_data = {
+          registerEmail : registerEmail,
+          registerUser : registerUser,
+          last_login : Date.now()
+        }
+        // Push to Firebase Database
+        database_ref.child('users/' + user.uid).set(user_data)
+        
+      })
+      
+      .catch(function(error) {
+        // Firebase will use this to alert of its errors
+        alert("error with fb database")
+        var error_code = error.code
+        var error_message = error.message
+        alert(error_message)
+      })
+      */
+      //FB
+  
+  ///////////////////////////////Valid Credentials ///////////////////////////////////////////
+  
+  if (registerUser.length < 3) {
+      alert("That username is too short.")
+      return
+  }
+  
+  else if (registerEmail.length < 3) {
+      alert("That email is too short.")
+      return
+  }
+  
+  else if(emailRegex(registerEmail) == false) {
+      alert("Invalid email.")
+      return
+  }
+  
+  else if (registerPassword.length < 3) {
+      alert("That password is too short.")
+      return
+  }
+  
+  for (i = 0; i <LoginInfo.length; i++){
+      if(registerUser==LoginInfo[i].username){
+          alert("That username is already in use, choose another")
+          return
+      }
+  }
+  
+  for (i = 0; i <LoginInfo.length; i++){
+      if(registerEmail==LoginInfo[i].email){
+          alert("That email is already in use, choose another")
+          return
+      }
+  }
+  
+  if (registerPassword == registerConfirmPassword)
+  {
+      var newUser = {
+          username: registerUser,
+          password: registerPassword,
+          email: registerEmail
+      
+      }
+      LoginInfo.push(newUser)
+      alert("Your Account has been created!")
+      
+      window.location.href = "https://mean-green-deal.github.io/content/userloggedin.html";
+      return
+  }
+  else{
+      alert("Passwords do not match. Try again")
+      return
+  }
+  
+  }
 function setFormMessage(formElement, type, message) {
     const messageElement = formElement.querySelector(".form__message");
 
@@ -49,212 +252,6 @@ function emailRegex(input) {
     return regex.test(input);
 }
 
-
-//retrieves user info for login
-function login() {
-    var email = document.getElementById("email").value
-    var password = document.getElementById("password").value
-
-    if (validate_email(email) == false || validate_password(password) == false) {
-        alert('Email or Password is Outta Line!!')
-        return
-        // Don't continue running the code
-      }
-    
-      auth.signInWithEmailAndPassword(email, password)
-      .then(function() {
-        // Declare user variable
-        var user = auth.currentUser
-    
-        // Add this user to Firebase Database
-        var database_ref = database.ref()
-    
-        // Create User data
-        var user_data = {
-          last_login : Date.now()
-        }
-    
-        // Push to Firebase Database
-        database_ref.child('users/' + user.uid).update(user_data)
-    
-        // DOne
-        alert('User Logged In!!')
-        window.location.href = "https://mean-green-deal.github.io/content/userloggedin.html";
-    })
-        .catch(function(error) {
-            // Firebase will use this to alert of its errors
-            var error_code = error.code
-            var error_message = error.message
-        
-            alert(error_message)
-            window.location.href = "https://mean-green-deal.github.io";
-          })
-    /*
-    for (i = 0; i <LoginInfo.length; i++) {
-        if (username == LoginInfo[i].username && password == LoginInfo[i].password) {
-            alert(username + " is logged in")
-          //FB
-          auth.signInWithEmailAndPassword(username, password)
-          .then(function() {
-            // Declare user variable
-            var user = auth.currentUser
-
-            // Add this user to Firebase Database
-            var database_ref = database.ref()
-
-            // Create User data
-            var user_data = {
-              last_login : Date.now()
-            }
-            // Push to Firebase Database
-            database_ref.child('users/' + user.uid).update(user_data)
-          })
-          .catch(function(error) {
-            // Firebase will use this to alert of its errors
-            var error_code = error.code
-            var error_message = error.message
-
-            alert(error_message)
-          })
-           window.location.href = "https://mean-green-deal.github.io/";
-            return
-        }
-    }
-    */
-    //console.log("incorrect username or password")
-}
-
-function registerUser() {
-  var registerUser = document.getElementById("newUser").value
-  var registerEmail = document.getElementById("newEmail").value
-  var registerPassword = document.getElementById("newPassword").value
-  var registerConfirmPassword = document.getElementById("confirmPassword").value
-  auth.createUserWithEmailAndPassword(registerEmail, registerPassword)
-  .then(function() {
-    // Declare user variable
-    var user = auth.currentUser
-
-    // Add this user to Firebase Database
-    var database_ref = database.ref()
-
-    // Create User data
-    var user_data = {
-      registerEmail : registerEmail,
-      registerUser : registerUser,
-      last_login : Date.now()
-    }
-    // Push to Firebase Database
-    database_ref.child('users/' + user.uid).set(user_data)
-  })
-  .catch(function(error) {
-    // Firebase will use this to alert of its errors
-    var error_code = error.code
-    var error_message = error.message
-    alert(error_message)
-  })
-
-
-
-
-    //FB
-    /*
-    auth.createUserWithEmailAndPassword(registerEmail, registerPassword)
-    .then(function(userCredential) {
-        const user = userCredential.user;
-        const usersRef = db.collection('users');
-        var user_data = {
-            registerEmail : registerEmail,
-            registerUser : registerUser,
-            last_login : Date.now()
-          };
-          usersRef.add(user_data)
-        
-      })
-        /*
-      // Declare user variable
-      
-      var user = auth.currentUser
-  
-      // Add this user to Firebase Database
-      var database_ref = database.ref()
-  
-      // Create User data
-      var user_data = {
-        registerEmail : registerEmail,
-        registerUser : registerUser,
-        last_login : Date.now()
-      }
-      // Push to Firebase Database
-      database_ref.child('users/' + user.uid).set(user_data)
-      
-    })
-    
-    .catch(function(error) {
-      // Firebase will use this to alert of its errors
-      alert("error with fb database")
-      var error_code = error.code
-      var error_message = error.message
-      alert(error_message)
-    })
-    */
-    //FB
-
-///////////////////////////////Valid Credentials ///////////////////////////////////////////
-
-if (registerUser.length < 3) {
-    alert("That username is too short.")
-    return
-}
-
-else if (registerEmail.length < 3) {
-    alert("That email is too short.")
-    return
-}
-
-else if(emailRegex(registerEmail) == false) {
-    alert("Invalid email.")
-    return
-}
-
-else if (registerPassword.length < 3) {
-    alert("That password is too short.")
-    return
-}
-
-for (i = 0; i <LoginInfo.length; i++){
-    if(registerUser==LoginInfo[i].username){
-        alert("That username is already in use, choose another")
-        return
-    }
-}
-
-for (i = 0; i <LoginInfo.length; i++){
-    if(registerEmail==LoginInfo[i].email){
-        alert("That email is already in use, choose another")
-        return
-    }
-}
-
-if (registerPassword == registerConfirmPassword)
-{
-    var newUser = {
-        username: registerUser,
-        password: registerPassword,
-        email: registerEmail
-    
-    }
-    LoginInfo.push(newUser)
-    alert("Your Account has been created!")
-    
-    window.location.href = "https://mean-green-deal.github.io/content/userloggedin.html";
-    return
-}
-else{
-    alert("Passwords do not match. Try again")
-    return
-}
-
-}
 ///////////////////////////////Valid Credentials///////////////////////////////////////////
 
 
