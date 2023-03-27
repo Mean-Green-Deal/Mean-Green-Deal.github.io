4cae1268b5655298aff69bd82f5c08766267d317
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-03-27 12:43:40 -0500

    award points

--- js/user.js
+++ js/user.js
@@ -259,12 +259,15 @@ function Recycle(map) {
 
   controlButton.addEventListener("click", () => {
     map.setCenter();
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
+    firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
-        const uid = user.uid;
+        var user_data = {
+          Points : Points+1
+        }
+        database_ref.child('users/' + user.uid).update(user_data)
+        alert("Congrats you are awarded 1 point.")
         // ...
       } else {
         // User is signed out
