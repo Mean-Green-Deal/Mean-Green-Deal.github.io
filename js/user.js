055a51c0969d018debae9eb14a3a1ba03a482e23
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-03-27 12:56:18 -0500

    db reference added

--- js/user.js
+++ js/user.js
@@ -260,6 +260,7 @@ function Recycle(map) {
   controlButton.addEventListener("click", () => {
     map.setCenter();
     firebase.auth().onAuthStateChanged((user) => {
+      var database_ref = database.ref()
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
