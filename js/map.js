8933601371e889f2946e32224719187bd4868f0f
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-04-19 11:13:47 -0500

    fixed map

--- js/map.js
+++ js/map.js
@@ -22,7 +22,7 @@ function initMap() {
         
      //directionsRenderer.setMap(map);
   });   
-
+}
     google.maps.event.addDomListener(window, "resize", function() {
       var center = map.getCenter();
       google.maps.event.trigger(map, "resize");
