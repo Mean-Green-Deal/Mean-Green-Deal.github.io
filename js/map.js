d8acee9d42d6c317c57aea44680da8cd68dc8d73
OzBirdog <116414879+OzBirdog@users.noreply.github.com>
2023-04-19 18:31:32 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -78,7 +78,7 @@ function initMap() {
                 iw.close();
             });
             infoWindow.open(map, marker);
-        });
+        };
      
     // Create the DIV to hold the control.
     const centerControlDiv = document.createElement("div");
