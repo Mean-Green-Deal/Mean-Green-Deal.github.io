cf22ab519bbc7d746ca1a7fc7e4daf6163a89178
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-06-09 20:49:46 -0500

    senmd email

---
+++ php/contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $Fname = $_POST['firstName']
+    $Lname = $_POST['lastName']
+    $Email = $_POST['studentEmail']
+    $Subject = $_POST['subject']
+
+    $mailTo = "meangreendeal@gmail.com";
+    $headers = "From: ".$Email;
+    $txt = "You have received an email from ".$Fname.".\n\n".$Subject;
+
+    mail($mailTo, "Mean-Green-Deal", $txt, $headers);
+    header("Location: index.php?mailsend");
+}
