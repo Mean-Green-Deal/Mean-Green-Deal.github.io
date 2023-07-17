2b457fd6b2a30101a0d934c780e76c88e0549c11
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-07-17 13:05:24 -0500

    fix php

--- php/contactform.php
+++ php/contactform.php
@@ -1,16 +1,26 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the form data
+    $firstName = $_POST["firstname"];
+    $lastName = $_POST["lastname"];
+    $email = $_POST["studentemail"];
+    $subject = $_POST["subject"];
 
-if(isset($_POST['Submit'])){
-    $Fname = $_POST['firstName']
-    $Lname = $_POST['lastName']
-    $Email = $_POST['studentEmail']
-    $Subject = $_POST['subject']
+    // Set up the email content
+    $to = $email; // Replace with the email address you want to receive the contact messages
+    $subject = "Contact Form Submission from $firstName $lastName";
+    $message = "Name: $firstName $lastName\n";
+    $message .= "Email: $email\n";
+    $message .= "Subject: $subject\n";
 
-    $mailTo = "meangreendeal@gmail.com";
-    $headers = "From: ".$Email;
-    $txt = "You have received an email from ".$Fname.".\n\n".$Subject;
+    // Set the From header to your business email address
+    $headers = "From: meangreendeal@gmail.com"; // Replace with your business email address
 
-    mail($mailTo, $Lname, $txt, $headers);
-    header("Location: contact.html?mailsend");
+    // Send the email
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Thank you for contacting us! We will get back to you soon.";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
 }
 ?>
