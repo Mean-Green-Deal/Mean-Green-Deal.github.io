08529c31408ea41fb10f60954b06aeebf1be4e9c
Scottyboi1 <scottavery2001@gmail.com>
2024-07-31 08:03:28 -0400

    moved bins

--- js/user.js
+++ js/user.js
@@ -1,82 +1,150 @@
-let map, infoWindow;
-let infoWindows = [];
-let binDirections = [];
+
+
+let map, infoWinow
+let infoWindows = []
+let binDirections  = []
+//FB Initializer
+
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 33.20750461273979, lng: -97.15295817275108 },
-    zoom: 14.7,
-    mapId: '576037077fe48406',
-    mapTypeControl: false,
-    fullscreenControl: false,
-    streetViewControl: false,
-    styles: [
-      {
-        "featureType": "poi",
-        "stylers": [
-          { "visibility": "off" }
-        ]
-      }
-    ]
-  });
-
-  google.maps.event.addDomListener(window, "resize", function() {
-    var center = map.getCenter();
-    google.maps.event.trigger(map, "resize");
-    map.setCenter(center);
-  });
-
-  const bins = [
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 33.20750461273979, lng: -97.15295817275108 },
+      zoom: 14.7,
+      mapid: '576037077fe48406',
+      mapTypeControl: false,
+      fullscreenControl: false,
+      streetViewControl: false,
+      styles: [
+        { 
+          "featureType": "poi", 
+          "stylers": [ 
+            { "visibility": "off" } 
+          ] 
+        }
+      ]
+    });
+    google.maps.event.addDomListener(window, "resize", function() {
+      var center = map.getCenter();
+      google.maps.event.trigger(map, "resize");
+      map.setCenter(center); 
+    });
+    /*
+   const bins = [
     ["Recycling Bin DP Out 1", 33.2555149, -97.1529043, "https://mean-green-deal.github.io/pictures/recycling bin.png"],
     ["Recycling Bin DP OUT 2", 33.2548833, -97.1533700, "https://mean-green-deal.github.io/pictures/recycling bin.png"],
     ["Recycling Bin DP IN 3", 33.2543800, -97.1536788, "https://mean-green-deal.github.io/pictures/recycling bin.png"],
-  ];
-
-  for (let i = 0; i < bins.length; i++) {
-    const bin = bins[i];
-    const marker = new google.maps.marker.AdvancedMarkerElement({
-      position: { lat: bin[1], lng: bin[2] },
-      map: map,
-      title: bin[0],
-      content: document.createElement('div'),
-      icon: {
-        url: bin[3],
-        size: new google.maps.Size(28.5, 23.25),
-        scaledSize: new google.maps.Size(28.5, 23.25)
-      }
-    });
-
-    const infoWindow = new google.maps.InfoWindow({
-      content: bin[0],
-    });
-
-    infoWindows.push(infoWindow);
-
-    marker.addListener("click", () => {
-      infoWindows.forEach((iw) => {
-        iw.close();
+    ];
+    */
+    const bins = [
+      [33.2555149, -97.1529043],
+      [33.2548833, -97.1533700],
+      [33.2543800, -97.1536788],
+      [33.2546405, -97.1535286],
+      [33.2541964, -97.1537884],
+      [33.2540419, -97.1528614],
+      [33.2530084, -97.1520399],
+      [33.2532607, -97.1524144],
+      [33.2531654, -97.1528657],
+      [33.2532955, -97.1527598],
+      [33.2543783, -97.1526464],
+      [33.2540410, -97.1524346],
+      [33.2533861, -97.1506733],
+      [33.2090314, -97.1477843],
+      [33.2089731, -97.1480699],
+      [33.2088620, -97.1480615],
+      [33.2085500, -97.1476441],
+      [33.2082095, -97.1482242],
+      [33.2088137, -97.1483073],
+      [33.2090435, -97.1482168],
+      [33.2091924, -97.1463922],
+      [33.2089456, -97.1470882],
+      [33.2555149, -97.1529043],
+      [33.2548833, -97.1533700],
+      [33.2543800, -97.1536788],
+      [33.2546405, -97.1535286],
+      [33.2541964, -97.1537884],
+      [33.2540419, -97.1528614],
+      [33.2530084, -97.1520399],
+      [33.2532607, -97.1524144],
+      [33.2531654, -97.1528657],
+      [33.2532955, -97.1527598],
+      [33.2543783, -97.1526464],
+      [33.2540410, -97.1524346],
+      [33.2533861, -97.1506733],
+      [33.2090314, -97.1477843],
+      [33.2089731, -97.1480699],
+      [33.2088620, -97.1480615],
+      [33.2085500, -97.1476441],
+      [33.2082095, -97.1482242],
+      [33.2088137, -97.1483073],
+      [33.2090435, -97.1482168],
+      [33.2091924, -97.1463922],
+      [33.2089456, -97.1470882],
+      [33.2097871, -97.1476220],
+      [33.2097882, -97.1474946],
+      [33.2100766, -97.1473856],
+      [33.2101372, -97.1473789],
+      [33.2102825, -97.1473910],
+      [33.2109417, -97.1473789],
+      [33.2106071, -97.1483824],
+      [33.2127530, -97.1485339],
+      [33.2101027, -97.1529643],
+      [33.2095843, -97.1542075],
+      [33.2103782, -97.1452449],
+      [33.2103397, -97.1463228],
+      [33.2104486, -97.1486342],
+      [33.2113588, -97.1476153],
+      [33.2114250, -97.1477756],
+      [33.2119322, -97.1484585],
+      [33.2117142, -97.1474624],
+      [33.2112194, -97.1464754],
+      [33.2116943, -97.1499391],
+      [33.2114640, -97.1509385],
+      [33.2114870, -97.1528060],
+      ];
+    for (let i = 0; i < bins.length; i++) {
+      const bin = bins[i];
+      const marker = new google.maps.Marker({
+        position: { lat: bin[1], lng: bin[2] },
+        map,
+        icon: {
+          url: bin[3],
+          scaledSize: new google.maps.Size(28.5, 23.25)
+        },
+        title: bin[0],
       });
-      infoWindow.open(map, marker);
-    });
-  }
-
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      const centerControlDiv = document.createElement("div");
-      const centerControl = RequestBin(map);
-      centerControlDiv.index = 1;
-      map.controls[google.maps.ControlPosition.TOP_RIGHT].push(centerControlDiv);
-
-      const customControlDiv1 = document.createElement("div");
-      var customControl1 = new Recycle(map);
-      customControl1.index = 2;
-      map.controls[google.maps.ControlPosition.TOP_LEFT].push(customControlDiv1);
-
-      centerControlDiv.appendChild(centerControl);
-      customControlDiv1.appendChild(customControl1);
+      const infoWindow = new google.maps.InfoWindow({
+        content: bin[0],
+      });
+      infoWindows.push(infoWindow);
+      marker.addListener("click", () => {
+        infoWindows.forEach((iw) => {
+          iw.close();
+        });
+        infoWindow.open(map, marker);
+      });
     }
+    
+      firebase.auth().onAuthStateChanged(function(user) {
+        // Create the DIV to hold the control.
+        if (user){
+          const centerControlDiv = document.createElement("div");
+          const centerControl = RequestBin(map);
+          centerControlDiv.index = 1;
+          map.controls[google.maps.ControlPosition.TOP_RIGHT].push(centerControlDiv);
+          const customControlDiv1 = document.createElement("div");
+          //const customControl1  = Recycle(map);
+          var customControl1 = new Recycle(map);
+          customControl1.index = 2;
+          map.controls[google.maps.ControlPosition.TOP_LEFT].push(customControlDiv1);
+          // Create the control.
+          // Append the control to the DIV.
+          centerControlDiv.appendChild(centerControl);
+          customControlDiv1.appendChild(customControl1);
+      }
   });
 }
+
 
 function getLocation() {
   /*
@@ -242,73 +310,6 @@ function Recycle(map) {
     controlButton.style.border = "2px solid #00853E";
     controlButton.style.cursor = "pointer";
   }
-  const bins = [
-    [33.2555149, -97.1529043],
-    [33.2548833, -97.1533700],
-    [33.2543800, -97.1536788],
-    [33.2546405, -97.1535286],
-    [33.2541964, -97.1537884],
-    [33.2540419, -97.1528614],
-    [33.2530084, -97.1520399],
-    [33.2532607, -97.1524144],
-    [33.2531654, -97.1528657],
-    [33.2532955, -97.1527598],
-    [33.2543783, -97.1526464],
-    [33.2540410, -97.1524346],
-    [33.2533861, -97.1506733],
-    [33.2090314, -97.1477843],
-    [33.2089731, -97.1480699],
-    [33.2088620, -97.1480615],
-    [33.2085500, -97.1476441],
-    [33.2082095, -97.1482242],
-    [33.2088137, -97.1483073],
-    [33.2090435, -97.1482168],
-    [33.2091924, -97.1463922],
-    [33.2089456, -97.1470882],
-    [33.2555149, -97.1529043],
-    [33.2548833, -97.1533700],
-    [33.2543800, -97.1536788],
-    [33.2546405, -97.1535286],
-    [33.2541964, -97.1537884],
-    [33.2540419, -97.1528614],
-    [33.2530084, -97.1520399],
-    [33.2532607, -97.1524144],
-    [33.2531654, -97.1528657],
-    [33.2532955, -97.1527598],
-    [33.2543783, -97.1526464],
-    [33.2540410, -97.1524346],
-    [33.2533861, -97.1506733],
-    [33.2090314, -97.1477843],
-    [33.2089731, -97.1480699],
-    [33.2088620, -97.1480615],
-    [33.2085500, -97.1476441],
-    [33.2082095, -97.1482242],
-    [33.2088137, -97.1483073],
-    [33.2090435, -97.1482168],
-    [33.2091924, -97.1463922],
-    [33.2089456, -97.1470882],
-    [33.2097871, -97.1476220],
-    [33.2097882, -97.1474946],
-    [33.2100766, -97.1473856],
-    [33.2101372, -97.1473789],
-    [33.2102825, -97.1473910],
-    [33.2109417, -97.1473789],
-    [33.2106071, -97.1483824],
-    [33.2127530, -97.1485339],
-    [33.2101027, -97.1529643],
-    [33.2095843, -97.1542075],
-    [33.2103782, -97.1452449],
-    [33.2103397, -97.1463228],
-    [33.2104486, -97.1486342],
-    [33.2113588, -97.1476153],
-    [33.2114250, -97.1477756],
-    [33.2119322, -97.1484585],
-    [33.2117142, -97.1474624],
-    [33.2112194, -97.1464754],
-    [33.2116943, -97.1499391],
-    [33.2114640, -97.1509385],
-    [33.2114870, -97.1528060],
-    ];
   // Set CSS for the control.
   controlButton.style.backgroundColor = "#00853E";
   controlButton.style.border = "2px solid #00853E";
